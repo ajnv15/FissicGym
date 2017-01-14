068d9d3165d1a57498e5f6cd1bba64d4f2a06ce2
Arturo Navarro <aj_nv@hotmail.com>
2017-01-14 14:38:25 -0600

    se agrega tabs

--- actualizar.php
+++ actualizar.php
@@ -1,22 +1,22 @@
 <?php
-include("conexion.php");
-$numero_original=$_POST['original'];
-$nombre=$_POST['nombre'];
-$numero=$_POST['numero'];
-$mes=$_POST['mes'];
-$dia=$_POST['dia'];
-$anio=$_POST['anio'];
+    include("conexion.php");
+    $numero_original=$_POST['original'];
+    $nombre=$_POST['nombre'];
+    $numero=$_POST['numero'];
+    $mes=$_POST['mes'];
+    $dia=$_POST['dia'];
+    $anio=$_POST['anio'];
 
 
-$mysql=mysql_query("UPDATE socios SET numero = '$numero', nombre='$nombre', mes='$mes', dia='$dia',anio='$anio' WHERE numero='$numero_original'");
+    $mysql=mysql_query("UPDATE socios SET numero = '$numero', nombre='$nombre', mes='$mes', dia='$dia',anio='$anio' WHERE numero='$numero_original'");
 
 
-if($mysql){
-header("location:versocios.php");
+    if($mysql){
+    header("location:versocios.php");
 
-echo "yes";
-}
-else{
-echo "NO SE PUDO".mysql_error();
-header("location:versocios.php");}
-?>
+    echo "yes";
+    }
+    else{
+    echo "NO SE PUDO".mysql_error();
+    header("location:versocios.php");}
+    ?>
